e49942b8ad4f501d20c9dbe18d555fc62acd1c96
Yahya-2009 <yahyapro856@gmail.com>
2025-10-15 20:01:49 -0400

    update2

---
+++ api/main.py
@@ -0,0 +1,32 @@
+#importing fastapi
+from fastapi import FastApi 
+
+# Initialize the FastAPI application 
+app = FastApi(
+    title="FastApi Example",
+    description="This is an example of using FastApi"
+)
+
+# define endpoints or routes
+@app.get('/')
+def default_route():
+    """
+    This is the defualt endpoint for this back-end.
+    """
+    return "you have just reached the default route. Back-end server is listening..
+
+
+@app.get("/example")
+def get_example():
+    """
+    This endpoint returns a JSON object consisting of a simple message.
+    """
+    return {"message": "Hello World!", "year":2025}
+
+
+@app.get("/example2")
+def get_example2(name):  #can also pass in paremeters
+    """
+    This endpoint takes in a parameter called "name"
+    """
+    return {"message": f"Hello World {name}!"}
